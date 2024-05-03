4ecb441d50e298304376787caf60660d89f1e0d3
HECTOR JAVIER PADILLA LARA <2609@rcarbonifera.tecnm.mx>
2024-05-03 08:54:26 -0600

    Metodo Deserializar

---
+++ PokemonQuiz/ViewModels/QuizViewModel.cs
@@ -0,0 +1,39 @@
+﻿
+using PokemonQuiz.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace PokemonQuiz.ViewModels
+{
+    public class QuizViewModel : INotifyPropertyChanged
+    {
+        private List<PokemonModel> Pokemones { get; set; } = new();
+        private List<PokemonModel> Distractores { get; set; } = new();
+
+        public ICommand IniciarJuegoCommand{ get; set; }
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public QuizViewModel() 
+        {
+            IniciarJuegoCommand = new Command(IniciarJuego);
+        }
+
+        private void IniciarJuego()
+        {
+            Shell.Current.GoToAsync("//Juego");
+
+        }
+        private void Deserializar()
+        {
+            Stream s = FileSystem.OpenAppPackageFileAsync("pokemon.json").Result;
+            var datos  =   JsonSerializer.Deserialize<List<PokemonModel>>(s);
+
+        }
+    }
+}
